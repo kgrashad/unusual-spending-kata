fca15da19f718d9861b22abc3f6144b1c33d2c63
Khaled Rashad <kras@live.com>
2021-05-14 13:54:18 +0400

    Add logic to return current month payments.

--- tracker.go
+++ tracker.go
@@ -1,8 +1,20 @@
 package main
 
-type Tracker struct{}
+type Tracker struct {
+	currentMonthPayments []Payment
+}
 type Payment struct{}
+type Month string
+
+const (
+	CurrentMonth  Month = "Current Month"
+	PreviousMonth Month = "Previous Month"
+)
+
+func (t *Tracker) TrackUserPayment(userId int, payment Payment, month Month) {
+	t.currentMonthPayments = append(t.currentMonthPayments, payment)
+}
 
 func (t *Tracker) GetUserPayments(userId int) (currMonth, prevMonth []Payment) {
-	return
+	return t.currentMonthPayments, []Payment{}
 }
--- tracker_test.go
+++ tracker_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestTracker(t *testing.T) {
 	t.Run("Zero payments should return empty list", func(t *testing.T) {
@@ -15,6 +18,23 @@ func TestTracker(t *testing.T) {
 
 		if len(prevMonth) != 0 {
 			t.Errorf("Previous month payments list expected to be empty, got %v", prevMonth)
+		}
+	})
+
+	t.Run("When one current month payment is tracker, GetUserPayments should return it.", func(t *testing.T) {
+		tracker := Tracker{}
+		payment := Payment{}
+		userId := 1
+
+		tracker.TrackUserPayment(userId, payment, CurrentMonth)
+
+		currMonthPayments, _ := tracker.GetUserPayments(userId)
+		want := []Payment{payment}
+
+		println(len(currMonthPayments))
+
+		if !reflect.DeepEqual(currMonthPayments, want) {
+			t.Errorf("got: %v, want: %v", currMonthPayments, want)
 		}
 	})
 }
