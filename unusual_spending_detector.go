6a49157b949988ad2ffc5d079c050e6a95407743
Khaled Rashad <kras@live.com>
2021-05-15 16:46:50 +0400

    Add DetectUnusualSpending logic.

--- unusual_spending_detector.go
+++ unusual_spending_detector.go
@@ -1,5 +1,15 @@
 package main
 
 func DetectUnusualSpending(input []PaymentCategorySummary) (result []PaymentCategorySummary) {
-	return []PaymentCategorySummary{}
+
+	result = make([]PaymentCategorySummary, 0)
+
+	for _, s := range input {
+
+		if (s.CurrentMonthTotal-s.PreviousMonthTotal)/s.PreviousMonthTotal >= 0.5 {
+			result = append(result, s)
+		}
+	}
+
+	return
 }
--- unusual_spending_detector_test.go
+++ unusual_spending_detector_test.go
@@ -10,4 +10,96 @@ func TestDetectUnusualSpending(t *testing.T) {
 		want := []PaymentCategorySummary{}
 		assertPaymentCategorySummaries(want, got, t)
 	})
+
+	t.Run("Equal payment total should return empty slice", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 100,
+				CurrentMonthTotal:  100,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := []PaymentCategorySummary{}
+		assertPaymentCategorySummaries(want, got, t)
+	})
+
+	t.Run("49.999999999% increase in payment total should return empty slice", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 100,
+				CurrentMonthTotal:  149.999999999,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := []PaymentCategorySummary{}
+		assertPaymentCategorySummaries(want, got, t)
+	})
+
+	t.Run("50% decrease in payment total should return empty slice", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 100,
+				CurrentMonthTotal:  50,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := []PaymentCategorySummary{}
+		assertPaymentCategorySummaries(want, got, t)
+	})
+
+	t.Run("50% increase in payment total should return summary", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 100,
+				CurrentMonthTotal:  150,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := summary
+		assertPaymentCategorySummaries(want, got, t)
+	})
+
+	t.Run("Zero total in previous month should return the expected result", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 0,
+				CurrentMonthTotal:  150,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := summary
+		assertPaymentCategorySummaries(want, got, t)
+	})
+
+	t.Run("Slice with mixed summaries should return ones with 50% spike only", func(t *testing.T) {
+		summary := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 200,
+				CurrentMonthTotal:  4000,
+			}, {
+				Category:           "Fun",
+				PreviousMonthTotal: 100,
+				CurrentMonthTotal:  50,
+			}, {
+				Category:           "Space Travel",
+				PreviousMonthTotal: 0,
+				CurrentMonthTotal:  0,
+			},
+		}
+		got := DetectUnusualSpending(summary)
+		want := []PaymentCategorySummary{
+			{
+				Category:           "Reading",
+				PreviousMonthTotal: 200,
+				CurrentMonthTotal:  4000,
+			},
+		}
+		assertPaymentCategorySummaries(want, got, t)
+	})
 }
